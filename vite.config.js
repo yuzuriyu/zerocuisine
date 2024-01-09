9141b4ea1d2df0b384a3d00284f1c53828ee75fd
cole <garcia.ccole@gmail.com>
2024-01-09 16:24:11 +0800

    modify vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext", // or a version of your choice
+    polyfillDynamicImport: false, // Disable dynamic import polyfill
+  },
+  esbuild: {
+    jsxFactory: "React.createElement", // or your JSX factory function
+    jsxFragment: "React.Fragment", // or your JSX fragment identifier
+  },
+});
