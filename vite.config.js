62c5ec473d74fa47c1cd06c2881a000e4b003822
cole <garcia.ccole@gmail.com>
2024-01-09 16:26:36 +0800

    add mime config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext", // or a version of your choice
-    polyfillDynamicImport: false, // Disable dynamic import polyfill
-  },
-  esbuild: {
-    jsxFactory: "React.createElement", // or your JSX factory function
-    jsxFragment: "React.Fragment", // or your JSX fragment identifier
+  server: {
+    mimeTypes: {
+      ".js": "application/javascript",
+    },
   },
 });
